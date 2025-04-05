6658659cf859ffa06fb418e6fee33eb6fed0ddfc
Impranjal1 <E23CSEU1443@bennett.edu.in>
2025-04-05 14:36:22 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Hero from "./component/Hero";
 import Login from "./component/Login";
 import "./index.css";
 import Dashboard from "./component/dashboard";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} /> {/* Updated to /dashboard */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/navbar" element={<Navbar />} />
+
       </Routes>
     </Router>
   );
